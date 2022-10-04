921be846ec3f983adddfd2d4fa1d35ab43503119
zdodson21 <zdodson21@gmail.com>
2022-10-04 13:16:33 -0400

    Completed getComputerChoice() method

--- script/rock-paper-scissors.js
+++ script/rock-paper-scissors.js
@@ -1,5 +1,18 @@
 function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) // Should get a random number between 0 & 2
+    if (randomNumber === 0) {
+        return "Rock"
+    } else if (randomNumber === 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+function playerSelection() {
     
 }
 
-getComputerChoice()
+function playGame(getComputerChoice, playerSelection) {
+
+}
